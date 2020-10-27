7a9d532d125c2ccb1ab1e1eae78aaea37fe2ff44
German <german.eksi@khk.ee>
2020-10-27 22:20:24 +0200

    3.4 lahendus

--- 1/3.4 Õunte jagamine.py
+++ 1/3.4 Õunte jagamine.py
@@ -1,4 +1,11 @@
-ounad = 14
-poisid = 7
-
-mitu_tahab = int(input("Mitu pöialpoissi saab õuna?: "))
+from random import *
+õun = int(input("Mitu pöialpoissi tahab õuna(0-7)? "))
+N = 0
+L = 0
+Algus_oun = 14
+while N < õun:
+    L = randint(1,2)
+    Algus_oun -= L
+    N += 1
+    print(L)
+print("Lumivalgekesel jäi " + str(Algus_oun))
