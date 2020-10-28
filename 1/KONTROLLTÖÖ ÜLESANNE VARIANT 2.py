441c4e9a2eb71aa03a897ce2bad1f8e7339b7a62
German <german.eksi@khk.ee>
2020-10-28 17:39:48 +0200

    KT VARIANT 2

---
+++ 1/KONTROLLTÖÖ ÜLESANNE VARIANT 2.py
@@ -0,0 +1,27 @@
+import random
+
+def konemaksumus(kone_maksumus):
+    return kone_maksumus
+
+#Siin on inputid
+koneminuti_hind = float(input("Sisestage oma kõneminuti hind:"))
+
+konede_koguarv = int(input("Sisestage oma kõnede arv:"))
+
+suvaline_arv = random.choice(konekestus)
+
+kone_maksumus = suvaline_arv * koneminuti_hind
+
+min_hind = 60
+
+max_hind = 600
+
+
+konekestus = []
+
+for i in range (10):
+    n = random.randint(1, 1001)
+    konekestus.append(n)
+    print(konekestus)
+
+
