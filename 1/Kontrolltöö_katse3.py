7042c84b03db08a12a95151e32096eb809abdc5c
German <german.eksi@khk.ee>
2020-12-16 18:00:03 +0200

    Python KT Basseiniotsad

--- 1/Kontrolltöö_katse3.py
+++ 1/Kontrolltöö_katse3.py
@@ -6,7 +6,7 @@ def Kilomeetrid(sammud, sammud_meeter):
     kilomeetrite_arv = (sammud * sammud_meeter) / 1000
     return(kilomeetrite_arv)
 
-sammud_meeter = int(input("Sisestage sammude pikkus meetrites: "))
+sammud_meeter = (input("Sisestage sammude pikkus meetrites: "))
 
 for i in range(7):                 #tsükkel, mis lisab suvalist 7 arvu vastavas vahemikus
     suvaline = randint(5000, 15000)
@@ -22,7 +22,7 @@ while ip < 7:
 print("Sammud arvutatud kilomeetriteks: ")    #tsükliga väljastab meetrid kilomeetritena
 i = 0
 while i < 7:
-    print(str(round((sammud[i] * sammud_meeter) / 1000)), "km")
+    print(int(round((sammud[i] * sammud_meeter) / 1000)), "km")
     i += 1
 
 Km_summa = (sum(sammud * sammud_meeter) / 1000)       #7 päeva läbitud kilomeetrite arv
